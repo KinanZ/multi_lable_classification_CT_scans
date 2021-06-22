a7517c7c02527531a356a808de0e9b6edffd6b01
KinanZ <kinan.z101@gmail.com>
2021-06-22 17:55:31 +0200

    adding my_utils

---
+++ src/my_utils.py
@@ -0,0 +1,35 @@
+import os
+import yaml
+
+
+def read_yaml(yaml_path):
+    """
+    Reads yaml file as dict.
+    """
+    with open(yaml_path) as f:
+        yaml_data = yaml.load(f, Loader=yaml.FullLoader)
+
+    return yaml_data
+
+
+def save_yaml(dict_file, yaml_path):
+    """
+    Saves a dict as yaml file.
+    """
+    with open(yaml_path, "w") as file:
+        documents = yaml.dump(dict_file, file)
+
+
+class Configuration:
+    current_file_path = os.path.dirname(os.path.realpath(__file__))
+    default_config_path = os.path.join(current_file_path, "configs", "default_config.yml")
+
+    def __init__(self, config_path: str = None, default_config_path=default_config_path):
+        # read base config
+        base_config = read_yaml(default_config_path)
+        if config_path is not None:
+            config = read_yaml(config_path)
+            # overwrite base config
+            base_config.update(config)
+        # set overwritten config
+        self.as_dict = base_config

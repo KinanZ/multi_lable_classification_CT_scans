d0ff21e6aeaf98fda36ba5a3c150c51e1e2a48ee
KinanZ <kinan.z101@gmail.com>
2021-07-10 18:50:38 +0200

    less gooo

--- job_bash_aug_10.sh
+++ job_bash_aug_10.sh
@@ -1,4 +1,4 @@
-#PBS -N supervised_MLC_aug_4
+#PBS -N supervised_MLC_aug_10
 #PBS -S /bin/bash
 #PBS -l nodes=1:ppn=8:gpus=1,mem=8gb,walltime=24:00:00
 #PBS -j oe
@@ -13,4 +13,4 @@ echo "pid, gpu_utilization [%], mem_utilization [%], max_memory_usage [MiB], tim
 nvidia-smi --query-accounted-apps="pid,gpu_util,mem_util,max_memory_usage,time" --format=csv | tail -n1
 
 echo 'Training Should start'
-python3 /misc/student/alzouabk/Thesis/supervised_multi_label_classification/src/train_aug_exp.py -cp '/misc/student/alzouabk/Thesis/supervised_multi_label_classification/configs/config_aug_4.yml'
+python3 /misc/student/alzouabk/Thesis/supervised_multi_label_classification/src/train_aug_exp.py -cp '/misc/student/alzouabk/Thesis/supervised_multi_label_classification/configs/config_aug_10.yml'
--- job_bash_aug_13.sh
+++ job_bash_aug_13.sh
@@ -1,4 +1,4 @@
-#PBS -N supervised_MLC_aug_5
+#PBS -N supervised_MLC_aug_13
 #PBS -S /bin/bash
 #PBS -l nodes=1:ppn=8:gpus=1,mem=8gb,walltime=24:00:00
 #PBS -j oe
@@ -13,4 +13,4 @@ echo "pid, gpu_utilization [%], mem_utilization [%], max_memory_usage [MiB], tim
 nvidia-smi --query-accounted-apps="pid,gpu_util,mem_util,max_memory_usage,time" --format=csv | tail -n1
 
 echo 'Training Should start'
-python3 /misc/student/alzouabk/Thesis/supervised_multi_label_classification/src/train_aug_exp.py -cp '/misc/student/alzouabk/Thesis/supervised_multi_label_classification/configs/config_aug_5.yml'
+python3 /misc/student/alzouabk/Thesis/supervised_multi_label_classification/src/train_aug_exp.py -cp '/misc/student/alzouabk/Thesis/supervised_multi_label_classification/configs/config_aug_13.yml'
